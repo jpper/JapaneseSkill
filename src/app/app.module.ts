bba288fc77d68c78f9ce3510bac69fdb3f6c5c6b
Jonathan Perry <jpper@umich.edu>
2019-05-08 13:33:08 +0900

    Set up the imports and providers to use angularfire in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFirestoreModule, FirestoreSettingsToken} from '@angular/fire/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxMoy7beZdNlo6Zzw-I3vl58M5fF-7v90',
-  authDomain: 'japan-info-jobs.firebaseapp.com',
-  databaseURL: 'https://japan-info-jobs.firebaseio.com',
-  projectId: 'japan-info-jobs',
-  storageBucket: 'japan-info-jobs.appspot.com',
-  messagingSenderId: '15907976341',
-  appId: '1:15907976341:web:77531a2c5f5551aa'
+    apiKey: 'AIzaSyAxMoy7beZdNlo6Zzw-I3vl58M5fF-7v90',
+    authDomain: 'japan-info-jobs.firebaseapp.com',
+    databaseURL: 'https://japan-info-jobs.firebaseio.com',
+    projectId: 'japan-info-jobs',
+    storageBucket: 'japan-info-jobs.appspot.com',
+    messagingSenderId: '15907976341',
+    appId: '1:15907976341:web:77531a2c5f5551aa'
 };
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        { provide: FirestoreSettingsToken, useValue: {} }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
