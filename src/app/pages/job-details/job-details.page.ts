a0671c448ca2d29a7dd16c0b9c5b290bf4ad6645
Jonathan Perry <jpper@umich.edu>
2019-05-08 14:26:11 +0900

    Imported ToastController and Router, and added a showToast function

--- src/app/pages/job-details/job-details.page.ts
+++ src/app/pages/job-details/job-details.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router } from '@angular/router';
+import {Job, JobService} from '../../services/job.service';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-job-details',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobDetailsPage implements OnInit {
 
-  constructor() { }
+  job: Job = {
+    title: '',
+    company: '',
+    industry: '',
+    location: '',
+    salary: ''
+  };
 
-  ngOnInit() {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private jobService: JobService,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) {
   }
 
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.jobService.getJob(id).subscribe(job => {
+        this.job = job;
+      });
+    }
+  }
+
+  showToast(msg) {
+    this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    }).then(toast => toast.present());
+  }
 }
