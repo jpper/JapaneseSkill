09bc09aae98f9a5adf5f7ddbeeeb01cbc7fcc18d
Jonathan Perry <jpper@umich.edu>
2019-05-13 11:56:52 +0900

    Added lodash

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Job, JobService } from '../services/job.service';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Job, JobService} from '../services/job.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-tab1',
@@ -9,11 +10,23 @@ import { Job, JobService } from '../services/job.service';
 })
 export class Tab1Page implements OnInit {
   private jobs: Observable<Job[]>;
+  queryText: string;
 
-  constructor(private jobService: JobService) { }
+  constructor(private jobService: JobService) {
+  }
 
   ngOnInit() {
     this.jobs = this.jobService.getJobs();
   }
+
+  /*
+  updateJobs() {
+    let queryTextLower = this.queryText.toLowerCase();
+    let filteredJobs = [];
+    let jobResults = _.filter(this.jobs, j => (j as any).title.toLowerCase().includes(queryTextLower));
+    if (jobResults) {
+      filteredJobs.push(jobResults);
+    }
+    */
 
 }
