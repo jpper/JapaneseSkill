fc3f8e52a6a6022548bf3180e09f00355c7ff21a
ji-mac06 <ji-mac06@ji-mac06s-iMac.local>
2019-05-14 13:27:51 +0900

    Set routing back to the tabs page being the default path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'job', loadChildren: './pages/job-details/job-details.module#JobDetailsPageModule' },
   { path: 'job/:id', loadChildren: './pages/job-details/job-details.module#JobDetailsPageModule' },
