2c3a2c5cfb9aed2c6c5e69d9e762df9b5b634543
Jonathan Perry <jpper@umich.edu>
2019-05-21 11:32:20 +0900

    Changed tab1 module name to JobsPageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import {AngularFireModule} from '@angular/fire';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 import { AngularFirestoreModule, FirestoreSettingsToken} from '@angular/fire/firestore';
 import { AngularFireAuthModule} from '@angular/fire/auth';
-import {Tab1Page} from './tab1/tab1.page';
 
 const firebaseConfig = {
     apiKey: 'AIzaSyAxMoy7beZdNlo6Zzw-I3vl58M5fF-7v90',
