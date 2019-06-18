15f96f3773f96a564dda014b315272841cf2e12b
Jonny Ramen <jpper@umich.edu>
2019-06-18 17:35:44 +0900

    Added code to print the hash key for use in the Facebook Developers app

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,10 +1,17 @@
 package io.ionic.starter;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
@@ -17,5 +24,19 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+
+    try {
+      PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+      for (Signature signature : info.signatures) {
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        md.update(signature.toByteArray());
+        String hashKey = new String(Base64.encode(md.digest(), 0));
+        Log.i("Applog", "printHashKey() Hash Key: " + hashKey);
+      }
+    } catch (NoSuchAlgorithmException e) {
+      Log.e("A string", "printHashKey()", e);
+    } catch (Exception e) {
+      Log.e("Give me the hash!!!", "printHashKey()", e);
+    }
   }
 }
