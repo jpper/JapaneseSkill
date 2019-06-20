21d77edd66b47ff445b9bb66e93494e7aacea20f
Jonathan Perry <jpper@umich.edu>
2019-06-20 13:47:07 +0900

    Got the tab1 page to display data from the NativeStorage data

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Deck, DeckService} from '../../services/deck.service';
 import {Observable} from 'rxjs';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,10 +14,36 @@ import {Observable} from 'rxjs';
 })
 export class Tab1Page implements OnInit {
   private decks: Observable<Deck[]>;
+  user: any;
+  userReady = false;
 
-  constructor(private deckService: DeckService) { }
 
-  ngOnInit() {
+  constructor(
+    private deckService: DeckService,
+    private fb: Facebook,
+    private nativeStorage: NativeStorage,
+    public loadingController: LoadingController,
+    private router: Router,
+) { }
+
+  async ngOnInit() {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...'
+    });
+    await loading.present();
+    this.nativeStorage.getItem('facebook_user')
+      .then(data => {
+        this.user = {
+          name: data.name,
+          email: data.email,
+          picture: data.picture
+        };
+        loading.dismiss();
+        this.userReady = true;
+      }, error => {
+        console.log(error);
+        loading.dismiss();
+      });
   }
 
   ionViewWillEnter() {
