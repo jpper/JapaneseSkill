3f075e6ae3e51c6247be0cbf21ff75693ab1b9e1
Jonathan Perry <jpper@umich.edu>
2019-06-20 11:30:06 +0900

    Added @ionic-native/native-storage to the project

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -17,9 +17,9 @@ export class LoginPage implements OnInit {
   loginWithFB() {
     this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
       this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
-        this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name']};
-      })
-    })
+        this.userData = { email: profile.email, first_name: profile.first_name, picture: profile.picture_large['data'].url, username: profile.name};
+      });
+    });
   }
 
 }
